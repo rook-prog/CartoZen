4467cc4bc7ec8514463ef23e64e69313f5c8b4a6
Srijith <153119525+rook-prog@users.noreply.github.com>
2025-08-12 11:41:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -288,10 +288,10 @@ if view == "Map":
                 label_col=lab,
                 label_fontsize=int(cluster_label_size),        # inset label size
                 label_color=inset_label_color,
-                label_align=inset_label_align,
+                label_align=None,
                 label_halo=inset_label_halo,
                 label_halo_width=float(inset_label_halo_w),
-                label_offset_px=(6,4),
+                #label_offset_px=(6,4),
                 anchor=cluster_anchor,
                 offset_frac=float(cluster_offset_frac),        # distance from cluster centroid (figure fraction)
                 frame_lw=float(cluster_frame_lw),              # inset frame thickness
